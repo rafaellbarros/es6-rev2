126a456658f17b18f01e15ccaf60dba718f8c7d7
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-08 23:44:24 -0200

    Initial commit - var vs let vs const

---
+++ 01-var-let-const.js
@@ -0,0 +1,15 @@
+function test() {
+    //var a = 'outro valor';
+    const xpto = 12;
+    
+    let a = 'outro valor';
+    
+    if (1 < 2) {
+        let a = 'valor';
+    }
+
+    console.log(xpto);
+    console.log(a);
+}
+
+test();
