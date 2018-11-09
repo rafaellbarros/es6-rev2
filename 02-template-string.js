6481dbc5712fb47b5a3050dee8da575455a4afaf
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-08 23:47:29 -0200

    Template string

---
+++ 02-template-string.js
@@ -0,0 +1,9 @@
+const msg = `
+<p>Quintouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+<p>Quintouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+<p>Quintouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+<p>Quintouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+<p>Comer é bom de mais júnior!!!</p>
+`;
+
+console.log(msg);
